7724a2dd8c588d7c980a5c76910af803f017fe73
RanJiayi <156782873+RanJiayi@users.noreply.github.com>
2024-03-04 19:14:16 +0800

    Add files via upload

---
+++ Practical4/recursive.py
@@ -0,0 +1,10 @@
+#The first three numbers of this sequence are 4,11, and 25
+a=4
+b=11
+c=25
+#the fourth number 
+d=2*c+3
+#the fifth number
+e=2*d+3
+#display the first five values
+print(a,b,c,d,e)
