906620e0bd473569c22a43b363407d1c1e1274d0
RanJiayi <156782873+RanJiayi@users.noreply.github.com>
2024-03-04 20:40:02 +0800

    Update culture_density.py

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -8,4 +8,6 @@ while x<90:
 	x=x*2
 #the number of days increase by one everyday
 	n=n+1
-print(f"On day {str(n)} the cell density goes over 90%.")
+#the maximum number of days for holiday = the number of days before the cell density passes 90
+m=n-1
+print(f"On day {str(n)} the cell density goes over 90%, and the maximum number of days for holiday is {str(m)}.")
