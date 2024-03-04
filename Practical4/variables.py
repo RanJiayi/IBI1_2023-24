37f29bcae572324bec166b1b1dec84d48d652688
RanJiayi <156782873+RanJiayi@users.noreply.github.com>
2024-03-04 20:34:02 +0800

    Update variables.py

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -3,7 +3,7 @@ b=36
 c=30
 d=a-b
 e=b-c
-#d<e,so a combination of running and strength training had a greater improvemrnt on running time
+#d=4,e=6,d<e,so a combination of running and strength training had a greater improvemrnt on running time
 
 '''X=True,Y=False,W='either X or Y'
 truth table:     X     Y     either X or Y
