395fc794e706dc592026640ec0a7c4c27cfad191
RanJiayi <156782873+RanJiayi@users.noreply.github.com>
2024-03-04 19:02:21 +0800

    Add files via upload

---
+++ Practical4/variables.py
@@ -0,0 +1,9 @@
+#a=40，b=36，c=30,d=a-b=4,e=b-c=6，d<e,so a combination of running and strength training had a greater improvemrnt on running time
+
+'''X=True,Y=False,W='either X or Y'
+truth table:     X     Y     either X or Y
+               True   True      False
+               True   False     True
+               False  True      True
+               False  False     False
+'''
