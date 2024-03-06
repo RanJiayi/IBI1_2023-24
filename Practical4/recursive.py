eb7d4e2fd758a098abd3052f3edefde04c0eab91
RanJiayi <156782873+RanJiayi@users.noreply.github.com>
2024-03-06 14:46:50 +0800

    Update recursive.py

--- Practical4/recursive.py
+++ Practical4/recursive.py
@@ -1,10 +1,10 @@
-#The first three numbers of this sequence are 4,11, and 25
+#The first three numbers of this sequence are a:4,b:11,c:and 25
 a=4
 b=11
 c=25
-#the fourth number = 2*(the third number)+3
+#d:the fourth number = 2*(the third number)+3
 d=2*c+3
-#the fifth number = 2*(the fourth number)+3
+#e:the fifth number = 2*(the fourth number)+3
 e=2*d+3
 #display the first five values
 print(a,b,c,d,e)
