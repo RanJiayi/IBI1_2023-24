84e59febfbb6f496e3a6965e16110a95e36e7c5f
RanJiayi <156782873+RanJiayi@users.noreply.github.com>
2024-03-08 11:32:32 +0800

    Add files via upload

---
+++ Practical4/recursive.py
@@ -0,0 +1,8 @@
+#a stores the numbers from the recursive sequence, and the first number is 4
+a=4
+#repeat five times to get five values
+for i in range (0,5):
+	#print the present value
+	print(a)
+	#calculate the next value
+	a=a*2+3
