2a6dbef928856527354d51c4b056356444ff6cac
RanJiayi <jiayi3.23@intl.zju.edu.cn>
2024-04-16 10:12:51 +0800

    modified

--- Practical6/average_day.py
+++ Practical6/average_day.py
@@ -1,15 +1,16 @@
-#create a dictionary to record the activities(key) and their time spent(value)
+#create and print a dictionary to record the activities(key) and their time spent(value)
 dic_day={'Sleeping':8,'Classes':6,'Studying':3.5,'TV':2,'Music':1,'other':3.5}
+print(dic_day)
 #the requested activity that can be modified
 activity_modified='Sleeping'
-print(dic_day[activity_modified])
+print(f'{activity_modified}:', dic_day[activity_modified])
 
 #Import the graph module
 import matplotlib.pyplot as plt
 #indicate the used data
-time_spent=[8,6,3.5,2,1,3.5]
+time_spent=list(dic_day.values())
 #add label descriptions
-activity=['Sleeping','Classes','Studying','TV','Music','other']
+activity=list(dic_day.keys())
 plt.figure()
 #construct a pie chart
 plt.pie(time_spent,labels=activity,startangle=90)
