3aa68876da60c8e471ac29075a99dafc25ce55f6
RanJiayi <jiayi3.23@intl.zju.edu.cn>
2024-04-16 08:52:20 +0800

    modified

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -7,6 +7,6 @@ while density<90:
 #the cell line doubles in density everyday
 	density*=2
 	day+=1
-#the maximum number of days for a holiday from the lab (exclude the first day and last day)
+#the maximum number of days for a holiday from the lab (exclude the first day and the last day)
 day_holiday=day-2
 print(f"On day {str(day)} the cell density goes over 90%, and the maximum number of days for holiday is {str(day_holiday)}.")
