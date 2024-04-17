d8f5c6f4bebe83351a165ef924a32ed6b589b95d
RanJiayi <jiayi3.23@intl.zju.edu.cn>
2024-04-17 15:55:46 +0800

    modified

--- Practical10/spatial_SIR.py
+++ Practical10/spatial_SIR.py
@@ -17,23 +17,21 @@ BETA=0.3
 #the recovery probability
 GAMMA=0.05
 
-# find infected points
+#repeat for 100 times
 for j in range(1,101):
 	infectedIndex = np.where(population==1)
-	# loop through all infected points
+	# find all infected points
 	for i in range(len(infectedIndex[0])):
-	    # get x, y coordinates for each point
 	    x = infectedIndex[0][i]
 	    y = infectedIndex[1][i]
-	    # infect each neighbour with probability beta
-	    # infect all 8 neighbours (this is a bit finicky, is there a better way?):
+	    #address all the neighbours
 	    for xNeighbour in range(x-1,x+2):
 	        for yNeighbour in range(y-1,y+2):
-	            # don't infect yourself! (Is this strictly necessary?)
+	            #exclude the initial point
 	            if (xNeighbour,yNeighbour) != (x,y):
-	                # make sure I don't fall off an edge
+	                #exclude the edge
 	                if xNeighbour != -1 and yNeighbour != -1 and xNeighbour!=100 and yNeighbour!=100:
-	                    # only infect neighbours that are not already infected!
+	                    #find the susceptible neighbours
 	                    if population[xNeighbour,yNeighbour]==0:
 	                        population[xNeighbour,yNeighbour]=np.random.choice(range(2),1,p=[1-BETA,BETA])[0]
 	#recovered individuals
